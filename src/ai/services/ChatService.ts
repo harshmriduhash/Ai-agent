729e7acf381f174b9892f0b7dbe0da3196e8b7d6
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-24 13:13:28 +0530

    ChatService

--- src/ai/services/ChatService.ts
+++ src/ai/services/ChatService.ts
@@ -3,41 +3,37 @@ import { HOST_URL_VERSION_PATH } from "../const";
 import { getDefaultAI } from "../utils/service";
 
 class ChatService {
-    apiBase:string;
+  apiBase: string;
 
-    constructor(){
-      const env = getDefaultAI();
-      if(!env){
-                this.apiBase = `${HOST_URL_VERSION_PATH}`;
-      }else{
-        this.apiBase = `${env.hostUrl}${env.appBasePath||""}`;
-      }
+  constructor() {
+    const env = getDefaultAI();
+    if (!env) {
+      this.apiBase = `${HOST_URL_VERSION_PATH}`;
+    } else {
+      this.apiBase = `${env.hostUrl}${env.appBasePath || ""}`;
     }
+  }
 
-    async invoke(payload: ChatPayload, controller: AbortController) {
-        return await fetch(this.apiBase+"/chat/completions", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-          signal: controller.signal,
-        });
-      }
-
+  async invoke(payload: ChatPayload, controller: AbortController) {
+    return await fetch(this.apiBase + "/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+      signal: controller.signal,
+    });
+  }
 }
 
 export default ChatService;
 
-let chatServiceInstance:ChatService;
+let chatServiceInstance: ChatService;
 
-export const createChatService = () : ChatService => {
+export const createChatService = (): ChatService => {
+  if (!chatServiceInstance) {
+    chatServiceInstance = new ChatService();
+  }
 
-    
-    if(!chatServiceInstance){
-        chatServiceInstance = new ChatService()
-    }
-
-    return chatServiceInstance;
-
-}
+  return chatServiceInstance;
+};
