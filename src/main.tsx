24b7743d0cbcaa80eb4a633c0f95deab98187f9b
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-25 11:56:28 +0530

    main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Ai from './ai/Ai'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Ai from "./ai/Ai";
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <Ai />    
+      <Ai />
     </Router>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
