3e5f7ce02257e2dd1e3b668faa5ebbebfdeac411
Harsh Mriduhash <Harshsahay2709@gmail.com>
2025-03-26 12:14:00 +0530

    aiAgent

--- src/ai/core/toolFunctions/RestToolFunction.ts
+++ src/ai/core/toolFunctions/RestToolFunction.ts
@@ -11,30 +11,34 @@ class RestToolFunction implements AgentToolFunction {
   toolName: string;
   toolInstance: Tool;
   authHeader: any = {};
-  constructor(id: string, toolName: string, toolInstance: Tool, env: Environment|undefined) {
+  constructor(
+    id: string,
+    toolName: string,
+    toolInstance: Tool,
+    env: Environment | undefined
+  ) {
     this.id = id;
     this.toolName = toolName;
     this.toolInstance = toolInstance;
-    if(env) {
+    if (env) {
       env.headers.forEach((header) => {
         this.authHeader[header.key] = header.value;
       });
-    }
-    else {
+    } else {
       const currentAuth: any = authHeaders.filter(
         (a) => a[toolName] != undefined
       );
-      console.log({currentAuth, authHeaders})
+      console.log({ currentAuth, authHeaders });
       if (currentAuth && currentAuth[0]) {
-          const auth = currentAuth[0];
-          if(auth && auth[toolName] && auth[toolName].token) {
-              console.log(auth[toolName])
-              this.authHeader = {
-                  'Gmheader': `Bearer ${auth[toolName].token}`,
-              };
-          }        
-      }  
-    }    
+        const auth = currentAuth[0];
+        if (auth && auth[toolName] && auth[toolName].token) {
+          console.log(auth[toolName]);
+          this.authHeader = {
+            Gmheader: `Bearer ${auth[toolName].token}`,
+          };
+        }
+      }
+    }
   }
 
   async fetchData(url: string, method: string, body: any): Promise<any> {
@@ -73,23 +77,22 @@ class RestToolFunction implements AgentToolFunction {
       { params }
     );
     console.log({ toolInstance: this.toolInstance });
-    let queryParams ="";
+    let queryParams = "";
     const { method, url, apiPath } = this.toolInstance;
 
-    if(Object.keys(params).length> 0){
-      queryParams = Object.keys(params).map((key) => {
-        return `${key}=${params[key]}`;
-      }
-      ).join("&");
-      if(apiPath?.includes("?")){
+    if (Object.keys(params).length > 0) {
+      queryParams = Object.keys(params)
+        .map((key) => {
+          return `${key}=${params[key]}`;
+        })
+        .join("&");
+      if (apiPath?.includes("?")) {
         queryParams = `&${queryParams}`;
-      }
-      else{
+      } else {
         queryParams = `?${queryParams}`;
       }
     }
 
-    
     let contentObj: any = {};
 
     if (url && method && apiPath) {
